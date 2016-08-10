74d2a499a5dbd15d543dfd8f95e5d92f305aa9a4
YannickDot <yannick.levif@gmail.com>
2016-08-10 10:06:58 +0200

    groundwork

---
+++ src/index.js
@@ -0,0 +1,1 @@
+// export {myLibrary} from './mylibrary/'
---
+++ test/test.js
@@ -0,0 +1,7 @@
+import {expect} from 'chai';
+
+describe('#myLibrary', function() {
+    it('should pass everytime', function() {
+        expect(true).to.equal(true);
+    });
+});
